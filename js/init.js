52d0aef84ec746175ad0b4c56f396b9a6902a542
Bradley Drake <brad@torchdevelopment.com>
2016-02-09 23:01:30 -0800

    Added skills section text, and preparing to add badges to the site.

--- js/init.js
+++ js/init.js
@@ -1,20 +1,8 @@
 (function($){
-	function smoothScroll (ele) {
-		var selector = '#' + ele.data('smooth-scroll');
-
-		$('html, body').animate({
-		    scrollTop: $(selector).offset().top
-		}, 1000);
-	}
-
   $(function(){
-
+    new WOW().init();
+    $('.scrollspy').scrollSpy();
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-    $('.smooth-scroll').on('click.smoothScroll', function (event) {
-    	event.preventDefault();
-    	smoothScroll($(this));
-    });
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
